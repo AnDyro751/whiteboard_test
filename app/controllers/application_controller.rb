5791717cde166c04181cf704a042bbd2f32d7709
Angel Mendez <angelmendezz751@gmail.com>
2021-05-17 20:36:21 -0500

    feat: Add devise and tailwindcss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,48 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  # @param [Proc] action
+  # @return [I18n] language
+  def switch_locale(&action)
+    if params[:locale]
+      set_locale_language(&action)
+    else
+      set_header_language(&action)
+    end
+  end
+
+  def valid_language?(lang)
+    I18n.available_locales.include?(lang)
+  end
+
+  # @return [I18n] language
+  # @param [Proc] action
+  def set_header_language(&action)
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{locale}'"
+    set_locale(locale, &action)
+  end
+
+  # @return [I18n] language
+  # @param [Proc] action
+  def set_locale_language(&action)
+    locale = params[:locale] || I18n.default_locale
+    set_locale(locale, &action)
+  end
+
+  # @param [String] locale
+  # @return [I18n] language
+  def set_locale(locale, &action)
+    I18n.with_locale(valid_language?(locale) ? locale : I18n.default_locale, &action)
+  end
+
+
+  private
+
+  # @return [String] Language header
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
 end
